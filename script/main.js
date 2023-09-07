9f2de1b22da88935352b7659cc7635ed5c6a114a
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2023-09-07 21:37:24 +0200

    grow on hover, anti scroll et le funny

--- script/main.js
+++ script/main.js
@@ -1,15 +1,26 @@
-function onMainLoad() {
-    // merci la doc firefox le sang
+window.onload = () => {
+    // Check if splashscreen was already seen (if not write a cookie)
     if (
         !document.cookie
             .split("; ")
             .find((row) => row.startsWith("doSomethingOnlyOnce"))
     ) {
         const expires = (new Date(Date.now() + 86400 * 1000)).toUTCString();
+        setTimeout(() => {
+            document.body.classList.remove("no-scroll");
+            document.getElementById("splashdiv").classList.add("displaynone");
+        }, 4500);
         document.cookie = `doSomethingOnlyOnce=true; expires=${expires}; SameSite=None; Secure`;
         console.log(document.cookie);
     }
     else {
         document.getElementById("splashdiv").classList.add("notransition");
+        document.body.classList.remove("no-scroll");
+        document.getElementById("splashdiv").classList.add("displaynone");
     }
+
+    // Add le funny
+    document.getElementById("#mainlogo").addEventListener("click", () => {
+        new Audio("./src/sound/funny.mp3").play();
+    });
 }
