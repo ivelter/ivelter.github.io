589be8eac249a560ad68cc5bb0684b52c1810d7b
Ilan Velter <ilan.velter@etu.umontpellier.fr>
2023-11-21 13:00:06 +0100

    stuff

--- script/skillDescription.js
+++ script/skillDescription.js
@@ -2,7 +2,16 @@ let previousElement = null;
 function showDescription(skillName) {
     const element = document.getElementById("skill" + skillName);
     element.classList.remove('displaynone');
-    if (previousElement != null) {
+    // badge lazy loading
+    if (skillName==="softskills" && document.getElementById("badgesSoft").children.length === 0) {
+        document.getElementById("badgesSoft").innerHTML = `
+            <iframe  frameborder="0"  scrolling="no" src="https://openbadgepassport.com/app/badge/info/642466/pic/embed" width="200" height="270" loading="lazy"></iframe>
+            <iframe  frameborder="0"  scrolling="no" src="https://openbadgepassport.com/app/badge/info/642468/pic/embed" width="200" height="270" loading="lazy"></iframe>
+            <iframe  frameborder="0"  scrolling="no" src="https://openbadgepassport.com/app/badge/info/642467/pic/embed" width="200" height="270" loading="lazy"></iframe>
+        `;
+    }
+
+    if (previousElement != null && element !== previousElement) {
         previousElement.classList.add('displaynone');
     }
     else {
@@ -11,3 +20,4 @@ function showDescription(skillName) {
     }
     previousElement = element;
 }
+
